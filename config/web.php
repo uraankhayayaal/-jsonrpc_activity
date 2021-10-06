81ea19d04142b864b47efe66a93ffb81675d5b85
Ayaal Kaplin <uraankhayayaal@gmail.com>
2021-10-06 09:59:35 +0300

    url manager updated

--- config/web.php
+++ config/web.php
@@ -48,6 +48,11 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '/' => 'site/index',
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<module:\w+>/<controller:\w+>/<action:\w+>/' => '<module>/<controller>/<action>',
             ],
         ],
     ],
